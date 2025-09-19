9c223104bec71aaa4ecf4ce8b451fd4973ce7326
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-09-19 22:38:00 +0000

    git subrepo clone git@github.com:pmalacho-mit/subrepo-dependency-management-example.git example
    subrepo:
      subdir:   "example"
      merged:   "14cb0c7"
    upstream:
      origin:   "git@github.com:pmalacho-mit/subrepo-dependency-management-example.git"
      branch:   "main"
      commit:   "14cb0c7"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "4f60dd7"

---
+++ example/dist/index.ts
@@ -0,0 +1,6 @@
+export const add = (a: number, b: number): number => a + b;
+export const subtract = (a: number, b: number): number => a - b;
+export const multiply = (a: number, b: number): number => a * b;
+export const divide = (a: number, b: number): number => a / b;
+export const exponentiate = (a: number, b: number): number => a ** b;
+export const square = (a: number): number => a * a;
