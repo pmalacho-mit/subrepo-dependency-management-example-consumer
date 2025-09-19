68ec767b393e02f7a49931892afd5e10eb7ca8f7
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-09-19 22:45:09 +0000

    setting up consumer

--- example/index.ts
+++ example/index.ts
@@ -4,3 +4,4 @@ export const multiply = (a: number, b: number): number => a * b;
 export const divide = (a: number, b: number): number => a / b;
 export const exponentiate = (a: number, b: number): number => a ** b;
 export const square = (a: number): number => a * a;
+export const cube = (a: number): number => a * a * a;
